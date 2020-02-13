d74c6d3aa202fc4d5a17fedac04e0ada0c8df64a
Jack-Copland <bonineb@gmail.com>
2020-02-13 17:19:31 +1100

    starting day 2

---
+++ src/day2.rs
@@ -0,0 +1,13 @@
+use crate::globalfuncs;
+
+pub fn part1() {
+    let mut numbers = globalfuncs::read_data("data/day2_input", ",");
+    numbers[1] = 12;
+    numbers[2] = 2;
+    let mut pos = 0;
+    
+}
+
+fn read_4() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod day1;
+mod day2;
 mod globalfuncs;
 fn main() {
-    day1::part1();
-    day1::part2();
+    // day1::part1();
+    // day1::part2();
+    day2::part1();
 }
 
 
